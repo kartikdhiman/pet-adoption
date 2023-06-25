2fd71428029153aee44686bf7e852bcbf40b005c
Kartik <hello@kumara.dev>
2023-06-25 14:15:42 +0530

    refactor: use DocumentFragment

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const petPromise = await fetch("https://learnwebcode.github.io/pet-adoption-data
 const pets = await petPromise.json();
 
 const template = document.querySelector("#animal-card");
-const wrapper = document.createElement("div");
+const wrapper = document.createDocumentFragment()
 
 pets.forEach((pet) => {
 	const clone = template.content.cloneNode(true);
